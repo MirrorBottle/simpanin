c65fd1d3e3b29d710559d2cec3e1640fff1741a3
ananta <anantayusra01@gmail.com>
2023-11-30 05:19:14 +0800

    feat (user : maintenance_detail) : buat tampilan

--- lib/pages/user/maintenance/maintenance_detail.dart
+++ lib/pages/user/maintenance/maintenance_detail.dart
@@ -1,15 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:simpanin/pages/user/maintenance/maintenance_list.dart';
 
 class UserMaintenanceDetailScreen extends StatefulWidget {
-  const UserMaintenanceDetailScreen({super.key});
+  final Task task;
+
+  const UserMaintenanceDetailScreen({Key? key, required this.task})
+      : super(key: key);
 
   @override
-  State<UserMaintenanceDetailScreen> createState() => _UserMaintenanceDetailScreenState();
+  State<UserMaintenanceDetailScreen> createState() =>
+      _UserMaintenanceDetailScreenState();
 }
 
-class _UserMaintenanceDetailScreenState extends State<UserMaintenanceDetailScreen> {
+class _UserMaintenanceDetailScreenState
+    extends State<UserMaintenanceDetailScreen> {
+  final _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFFeFeFeF),
+      appBar: AppBar(
+        toolbarHeight: 0,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        titleSpacing: 0.0,
+        iconTheme: const IconThemeData(color: Colors.white),
+        automaticallyImplyLeading: false,
+        scrolledUnderElevation: 0,
+      ),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        reverse: true,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: BackButton(
+                color: Theme.of(context).colorScheme.primary,
+                onPressed: () {
+                  Navigator.pop(context); // Pop this screen when the back button is pressed
+                },
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Task Detail',
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFF16807),
+                      fontSize: 30,
+                    ),
+              ),
+              subtitle: Text(
+                'User Information Maintenance',
+                style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height - 160,
+              ),
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(32),
+                  topLeft: Radius.circular(32),
+                ),
+                color: Theme.of(context).colorScheme.background,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Nama: ${widget.task.nama}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'Deadline: ${DateFormat('yyyy-MM-dd').format(widget.task.deadline)}',
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  // Add other task details as needed
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
