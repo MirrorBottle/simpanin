97fb728c942a0606f20469c59b51c9416d316cb9
MirrorBottle <setiawanbayu66152@gmail.com>
2023-11-29 17:54:35 +0800

    chore (global): faq detail

---
+++ lib/pages/profile/profile_faq_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileFaqDetailScreen extends StatefulWidget {
+  const ProfileFaqDetailScreen({super.key});
+
+  @override
+  State<ProfileFaqDetailScreen> createState() => _ProfileFaqDetailScreenState();
+}
+
+class _ProfileFaqDetailScreenState extends State<ProfileFaqDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
