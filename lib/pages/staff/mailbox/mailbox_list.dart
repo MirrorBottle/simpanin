7b7beac09e06df6c57ab76518c932cbafaf7f532
awansetyawan <alifm6331@gmail.com>
2023-12-05 19:51:16 +0800

    feat (mailbox) : create mailbox

--- lib/pages/staff/mailbox/mailbox_list.dart
+++ lib/pages/staff/mailbox/mailbox_list.dart
@@ -27,112 +27,120 @@ class _StaffMailboxListScreenState extends State<StaffMailboxListScreen> {
       resizeToAvoidBottomInset: false,
       backgroundColor: Theme.of(context).colorScheme.tertiary,
       body: StreamBuilder<QuerySnapshot>(
-        stream: db
-            .collection('mailboxes')
-            .orderBy("code")
-            .snapshots(),
-        builder: (context, snapshot) {
-          return SingleChildScrollView(
-            controller: _scrollController,
-            reverse: true,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 100),
-                  child: Text("Mailbox",
-                      style: Theme.of(context)
-                          .textTheme
-                          .displayLarge!
-                          .copyWith(color: Theme.of(context).colorScheme.primary)),
-                ),
-                const SizedBox(height: 35),
-                Container(
-                  constraints:
-                      BoxConstraints(minHeight: MediaQuery.of(context).size.height - 255),
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(32),
-                        topLeft: Radius.circular(32)),
-                    color: Theme.of(context).colorScheme.background,
+          stream: db.collection('mailboxes').orderBy("code").snapshots(),
+          builder: (context, snapshot) {
+            return SingleChildScrollView(
+              controller: _scrollController,
+              reverse: true,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, top: 100),
+                    child: Text("Mailbox",
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayLarge!
+                            .copyWith(
+                                color: Theme.of(context).colorScheme.primary)),
                   ),
-                  child: snapshot.hasData
-                      ? SizedBox(
-                        height: 100,
-                        child: ListView(
-                            children: snapshot.data!.docs.map((doc) {
-                              MailboxModel mailbox =
-                                  MailboxModel.fromFirestore(doc);
-                              return Column(
-                                children: [
-                                  ListTile(
-                                    onLongPress: () { 
-                                    },
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                StaffMailboxDetailScreen(
-                                                    mailbox: mailbox)),
-                                      );
-                                    },
-                                    leading: Container(
-                                      height: 60,
-                                      width: 60,
-                                      decoration: BoxDecoration(
-                                        color: mailbox.availability ? Colors.green  : Theme.of(context)
+                  const SizedBox(height: 35),
+                  Container(
+                    constraints: BoxConstraints(
+                        minHeight: MediaQuery.of(context).size.height - 255),
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(32),
+                          topLeft: Radius.circular(32)),
+                      color: Theme.of(context).colorScheme.background,
+                    ),
+                    child: snapshot.hasData
+                        ? SizedBox(
+                            height: 100,
+                            child: ListView(
+                              children: snapshot.data!.docs.map((doc) {
+                                MailboxModel mailbox =
+                                    MailboxModel.fromFirestore(doc);
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                      onLongPress: () {},
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  StaffMailboxDetailScreen(
+                                                      mailbox: mailbox)),
+                                        );
+                                      },
+                                      leading: Container(
+                                        height: 60,
+                                        width: 60,
+                                        decoration: BoxDecoration(
+                                          color: mailbox.availability
+                                              ? Colors.green
+                                              : Theme.of(context)
+                                                  .colorScheme
+                                                  .tertiary,
+                                          shape: BoxShape.circle,
+                                        ),
+                                        alignment: Alignment.center,
+                                        child: Center(
+                                          child: Text(mailbox.code,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .displayMedium
+                                                  ?.copyWith(
+                                                      color: mailbox
+                                                              .availability
+                                                          ? Colors.white
+                                                          : Theme.of(context)
+                                                              .colorScheme
+                                                              .primary)),
+                                        ),
+                                      ),
+                                      title: Text(mailbox.formattedPrice,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleLarge),
+                                      subtitle: Text("Ukuran ${mailbox.size}",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyLarge!
+                                              .copyWith(color: Colors.grey)),
+                                      trailing: Icon(
+                                        Iconsax.arrow_right,
+                                        color: Theme.of(context)
                                             .colorScheme
-                                            .tertiary,
-                                        shape: BoxShape.circle,
-                                      ),
-                                      alignment: Alignment.center,
-                                      child: Center(
-                                        child: Text(mailbox.code,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .displayMedium
-                                                ?.copyWith(
-                                                    color: mailbox.availability ? Colors.white  : Theme.of(context)
-                                                        .colorScheme
-                                                        .primary)),
+                                            .secondary,
+                                        size: 28,
                                       ),
                                     ),
-                                    title: Text(mailbox.formattedPrice,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleLarge),
-                                    subtitle: Text("Ukuran ${mailbox.size}",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyLarge!.copyWith(color: Colors.grey)),
-                                    trailing: Icon(
-                                      Iconsax.arrow_right,
-                                      color:
-                                          Theme.of(context).colorScheme.secondary,
-                                      size: 28,
-                                    ),
-                                  ),
-                                ],
-                              );
-                            }).toList(),
-                          ),
-                      )
-                      : (const Center(
-                          child: CircularProgressIndicator(),
-                        )),
-                ),
-              ],
-            ),
-          );
-        }
-      ),
+                                  ],
+                                );
+                              }).toList(),
+                            ),
+                          )
+                        : (const Center(
+                            child: CircularProgressIndicator(),
+                          )),
+                  ),
+                ],
+              ),
+            );
+          }),
       // FloatingActionButton dengan label "Tambah"
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const StaffMailboxCreateScreen()),
+          );
         },
         tooltip: 'Tambah',
         label: const Text("Tambah", style: TextStyle(color: Colors.white)),
