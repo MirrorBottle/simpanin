1bc96a65b2ba67fe8b398f5f21c58bd52cd13061
MirrorBottle <setiawanbayu66152@gmail.com>
2023-12-05 18:06:30 +0800

    fix (staff): container maintenance

--- lib/pages/staff/maintenance/maintenance_list.dart
+++ lib/pages/staff/maintenance/maintenance_list.dart
@@ -18,8 +18,10 @@ class Task {
   final String nama;
   final DateTime deadline;
   final int prioritas;
-  final DateTime? tanggalMulai; // Tambahkan tanda tanya (?) untuk mengizinkan nilai null
-  final DateTime? tanggalSelesai; // Tambahkan tanda tanya (?) untuk mengizinkan nilai null
+  final DateTime?
+      tanggalMulai; // Tambahkan tanda tanya (?) untuk mengizinkan nilai null
+  final DateTime?
+      tanggalSelesai; // Tambahkan tanda tanya (?) untuk mengizinkan nilai null
   final String nomorHP;
   final String catatan;
   bool selesai;
@@ -44,63 +46,28 @@ class _StaffMaintenanceListScreenState
 
   @override
   Widget build(BuildContext context) {
-    int totalTugas = tasks.length;
-    int totalPrioritas1 = tasks.where((task) => task.prioritas == 1).length;
-    int totalPrioritas2 = tasks.where((task) => task.prioritas == 2).length;
-    int totalPrioritas3 = tasks.where((task) => task.prioritas == 3).length;
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Color(0xFFeFeFeF),
-      appBar: AppBar(
-        toolbarHeight: 0,
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        titleSpacing: 0.0,
-        iconTheme: const IconThemeData(color: Colors.white),
-        automaticallyImplyLeading: false,
-        scrolledUnderElevation: 0,
-      ),
+      backgroundColor: Theme.of(context).colorScheme.tertiary,
       body: SingleChildScrollView(
         controller: _scrollController,
         reverse: true,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: BackButton(
-                color: Theme.of(context).colorScheme.primary,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ProfileScreen()),
-                  );
-                },
-              ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, top: 100),
+              child: Text("Maintenance",
+                  style: Theme.of(context)
+                      .textTheme
+                      .displayLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.primary)),
             ),
-            ListTile(
-              title: Text(
-                "Maintenance",
-                style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFFF16807),
-                      fontSize: 30,
-                    ),
-              ),
-              subtitle: Text(
-                "Staff Maintenance List",
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      color: Color.fromARGB(255, 0, 0, 0),
-                    ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
+            const SizedBox(height: 35),
             Container(
-              constraints: BoxConstraints(
-                  minHeight: MediaQuery.of(context).size.height - 160),
+              constraints:
+                  BoxConstraints(minHeight: MediaQuery.of(context).size.height - 255),
               width: MediaQuery.of(context).size.width,
               padding: const EdgeInsets.all(20.0),
               decoration: BoxDecoration(
@@ -109,85 +76,7 @@ class _StaffMaintenanceListScreenState
                     topLeft: Radius.circular(32)),
                 color: Theme.of(context).colorScheme.background,
               ),
-              child: ListView.builder(
-                // agar menangkap semua element di dalam singelchild untuk dapat di sroll menjadi satu
-                shrinkWrap: true,
-                // agar tidak perlu mengetuk container di atas listview terlebih dahulu ssebelum bisa di scroll,sehingga seluruh bagian pada halaman bisa di scroll
-                physics: const NeverScrollableScrollPhysics(),
-                // total jumlah / panjang list
-                itemCount: tasks.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                    children: [
-                      ListTile(
-                        contentPadding: EdgeInsets.symmetric(vertical: 5),
-                        onTap: () {
-                          // Navigasi ke halaman detail dengan membawa data task
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  StaffMaintenanceDetailScreen(
-                                      task: tasks[index]),
-                            ),
-                          );
-                        },
-                        leading: Container(
-                          height: 50,
-                          width: 50,
-                          margin: const EdgeInsets.only(right: 7),
-                          decoration: BoxDecoration(
-                            color: tasks[index].prioritas == 1
-                                ? Theme.of(context).colorScheme.primary
-                                : tasks[index].prioritas == 2
-                                    ? const Color.fromARGB(255, 29, 161, 101)
-                                    : Colors.blue,
-                            shape: BoxShape.circle,
-                          ),
-                          child: Center(
-                            child: Icon(
-                              tasks[index].prioritas == 1
-                                  ? Iconsax
-                                      .message_time // Ganti dengan ikon prioritas tertinggi
-                                  : tasks[index].prioritas == 2
-                                      ? Iconsax
-                                          .tick_circle // Ganti dengan ikon prioritas sedang
-                                      : Iconsax
-                                          .receipt_edit, // Ganti dengan ikon prioritas rendah
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        title: Text(
-                          tasks[index].nama,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                        subtitle: Text(
-                          DateFormat('yyyy-MM-dd')
-                              .format(tasks[index].deadline),
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15),
-                        ),
-                        trailing: const Icon(
-                          Iconsax.arrow_right,
-                          color: Color.fromARGB(255, 32, 23, 23),
-                          size: 22,
-                        ),
-                      ),
-                      const Divider(
-                        height: 0,
-                        thickness: 0.8,
-                        indent: 65,
-                        endIndent: 0,
-                        color: Color.fromARGB(96, 72, 72, 72),
-                      ),
-                    ],
-                  );
-                },
-              ),
+              child: Text("test"),
             ),
           ],
         ),
