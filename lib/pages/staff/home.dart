b51e1e1a409f1838bc0ef6e8654f1d55d30b33ce
MirrorBottle <setiawanbayu66152@gmail.com>
2023-12-07 15:13:47 +0800

    fix (staff): booking list and payment list

--- lib/pages/staff/home.dart
+++ lib/pages/staff/home.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:simpanin/components/button_component.dart';
+import 'package:simpanin/models/agreement.dart';
+import 'package:simpanin/models/payment.dart';
+import 'package:simpanin/models/user.dart';
 
 class StaffHomeScreen extends StatefulWidget {
   const StaffHomeScreen({super.key});
@@ -11,6 +15,8 @@ class StaffHomeScreen extends StatefulWidget {
 }
 
 class _StaffHomeScreenState extends State<StaffHomeScreen> {
+  final db = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,16 +44,6 @@ class _StaffHomeScreenState extends State<StaffHomeScreen> {
                         "Booking",
                         style: Theme.of(context).textTheme.displayMedium,
                       ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Text(
-                          "Lihat Semua",
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleMedium!
-                              .copyWith(decoration: TextDecoration.underline),
-                        ),
-                      )
                     ],
                   ),
                 ],
@@ -56,61 +52,91 @@ class _StaffHomeScreenState extends State<StaffHomeScreen> {
             const SizedBox(
               height: 10,
             ),
-            ListTile(
-              leading: Container(
-                height: 70,
-                width: 70,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  shape: BoxShape.circle,
-                ),
-                alignment: Alignment.center,
-                child: Center(
-                  child: Text("C2",
-                      style: Theme.of(context)
-                          .textTheme
-                          .displayMedium
-                          ?.copyWith(
-                              color: Theme.of(context).colorScheme.primary)),
-                ),
-              ),
-              title: Text("Bayu Setiawan",
-                  style: Theme.of(context).textTheme.titleLarge),
-              subtitle:
-                  Text("22 Nov", style: Theme.of(context).textTheme.bodyLarge),
-              trailing: const Icon(
-                Iconsax.arrow_right,
-                color: Color.fromARGB(255, 32, 23, 23),
-                size: 22,
-              ),
-            ),
-            ListTile(
-              leading: Container(
-                height: 70,
-                width: 70,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  shape: BoxShape.circle,
-                ),
-                alignment: Alignment.center,
-                child: Center(
-                  child: Text("C3",
-                      style: Theme.of(context)
-                          .textTheme
-                          .displayMedium
-                          ?.copyWith(
-                              color: Theme.of(context).colorScheme.primary)),
-                ),
-              ),
-              title: Text("Ananta Yusra",
-                  style: Theme.of(context).textTheme.titleLarge),
-              subtitle:
-                  Text("22 Nov", style: Theme.of(context).textTheme.bodyLarge),
-              trailing: const Icon(
-                Iconsax.arrow_right,
-                color: Color.fromARGB(255, 32, 23, 23),
-                size: 22,
-              ),
+            StreamBuilder<QuerySnapshot>(
+              stream: db
+                  .collection('payments')
+                  .where("is_booking", isEqualTo: true)
+                  .orderBy("date", descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                return snapshot.hasData
+                    ? SizedBox(
+                        height: 100,
+                        child: ListView(
+                          children: snapshot.data!.docs.map((doc) {
+                            return FutureBuilder(
+                                future: Future.wait<dynamic>([
+                                  doc['agreement'].get(),
+                                  doc['mailbox'].get(),
+                                ]),
+                                builder: (BuildContext context,
+                                    AsyncSnapshot<List<dynamic>> snapshot) {
+                                  if (snapshot.connectionState ==
+                                      ConnectionState.waiting) {
+                                    return const Text('');
+                                  }
+                                  final payment = PaymentModel.fromFuture(doc,
+                                      snapshot.data![0], snapshot.data![1]);
+                                  return FutureBuilder(
+                                      future: Future.wait<dynamic>([
+                                        snapshot.data![0]['user'].get(),
+                                      ]),
+                                      builder: (BuildContext ctx,
+                                          AsyncSnapshot<List<dynamic>>
+                                              snapshot) {
+                                        if (snapshot.connectionState ==
+                                            ConnectionState.waiting) {
+                                          return const Text('');
+                                        }
+                                        final user = UserModel.fromFirestore(snapshot.data![0]);
+                                        return ListTile(
+                                          leading: Container(
+                                            height: 70,
+                                            width: 70,
+                                            decoration: BoxDecoration(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .tertiary,
+                                              shape: BoxShape.circle,
+                                            ),
+                                            alignment: Alignment.center,
+                                            child: Center(
+                                              child: Text(payment.mailbox.code,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .displayMedium
+                                                      ?.copyWith(
+                                                          color:
+                                                              Theme.of(context)
+                                                                  .colorScheme
+                                                                  .primary)),
+                                            ),
+                                          ),
+                                          title: Text(
+                                              user.name,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .titleLarge),
+                                          subtitle: Text(payment.formattedDate,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyLarge),
+                                          trailing: const Icon(
+                                            Iconsax.arrow_right,
+                                            color:
+                                                Color.fromARGB(255, 32, 23, 23),
+                                            size: 22,
+                                          ),
+                                        );
+                                      });
+                                });
+                          }).toList(),
+                        ),
+                      )
+                    : (const Center(
+                        child: CircularProgressIndicator(),
+                      ));
+              },
             ),
           ],
         ),
