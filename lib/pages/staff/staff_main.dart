dc5e67cf56afcc0f95e9dfbd47b946b4c9a1ebf5
MirrorBottle <setiawanbayu66152@gmail.com>
2023-12-07 15:53:12 +0800

    fix (dark): user and staff main

--- lib/pages/staff/staff_main.dart
+++ lib/pages/staff/staff_main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 import 'package:simpanin/pages/profile/profile.dart';
 import 'package:simpanin/pages/staff/home.dart';
 import 'package:simpanin/pages/staff/mailbox/mailbox_list.dart';
 import 'package:simpanin/pages/staff/maintenance/maintenance_list.dart';
 import 'package:simpanin/pages/staff/payment/payment_list.dart';
+import 'package:simpanin/providers/theme_mode_provider.dart';
 
 class StaffMainScreen extends StatefulWidget {
   const StaffMainScreen({Key? key}) : super(key: key);
@@ -31,49 +33,55 @@ class _StaffMainScreenState extends State<StaffMainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: tabItems[_selectedIndex],
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-        color: const Color(0xFFFAFAFA),
-        child: SalomonBottomBar(
-          currentIndex: _selectedIndex,
-          onTap: (i) => setState(() => _selectedIndex = i),
-          items: [
-            /// Home
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.home),
-              title: const Text("Home"),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
+    return Consumer<ThemeModeProvider>(
+        builder: (context, themeModeProvider, child) {
+      return Scaffold(
+        body: tabItems[_selectedIndex],
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+          color: themeModeProvider.isDarkModeActive
+              ? Color(0xFF2D2D2D)
+              : Color(0xFFFAFAFA),
+          child: SalomonBottomBar(
+            currentIndex: _selectedIndex,
+            onTap: (i) => setState(() => _selectedIndex = i),
+            items: [
+              /// Home
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.home),
+                title: const Text("Home"),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
 
-            /// Likes
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.box),
-              title: const Text("Mailbox"),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
+              /// Likes
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.box),
+                title: const Text("Mailbox"),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
 
-            /// Search
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.broom),
-              title: const Text("Maint."),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.send),
-              title: const Text("Pemb."),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
-            /// Profile
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.user),
-              title: const Text("Profile"),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
-          ],
+              /// Search
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.broom),
+                title: const Text("Maint."),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.send),
+                title: const Text("Pemb."),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
+
+              /// Profile
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.user),
+                title: const Text("Profile"),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
--- lib/pages/user/user_main.dart
+++ lib/pages/user/user_main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 import 'package:simpanin/pages/profile/profile.dart';
 import 'package:simpanin/pages/user/home.dart';
 import 'package:simpanin/pages/user/mailbox/mailbox_list.dart';
 import 'package:simpanin/pages/user/payment/payment_list.dart';
+import 'package:simpanin/providers/theme_mode_provider.dart';
 
 class UserMainScreen extends StatefulWidget {
   const UserMainScreen({Key? key}) : super(key: key);
@@ -29,45 +31,47 @@ class _UserMainScreenState extends State<UserMainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: tabItems[_selectedIndex],
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-        color: const Color(0xFFFAFAFA),
-        child: SalomonBottomBar(
-          currentIndex: _selectedIndex,
-          onTap: (i) => setState(() => _selectedIndex = i),
-          items: [
-            /// Home
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.home),
-              title: const Text("Beranda"),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
+    return Consumer<ThemeModeProvider>(builder: (context, themeModeProvider, child) {
+      return Scaffold(
+        body: tabItems[_selectedIndex],
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          color: themeModeProvider.isDarkModeActive ?  Color(0xFF2D2D2D) : Color(0xFFFAFAFA),
+          child: SalomonBottomBar(
+            currentIndex: _selectedIndex,
+            onTap: (i) => setState(() => _selectedIndex = i),
+            items: [
+              /// Home
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.home),
+                title: const Text("Beranda"),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
 
-            /// Likes
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.box),
-              title: const Text("Mailbox"),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
+              /// Likes
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.box),
+                title: const Text("Mailbox"),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
 
-            /// Search
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.send),
-              title: const Text("Pembayaran"),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
+              /// Search
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.send),
+                title: const Text("Pembayaran"),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
 
-            /// Profile
-            SalomonBottomBarItem(
-              icon: const Icon(Iconsax.user),
-              title: const Text("Profil"),
-              selectedColor: Theme.of(context).colorScheme.primary,
-            ),
-          ],
+              /// Profile
+              SalomonBottomBarItem(
+                icon: const Icon(Iconsax.user),
+                title: const Text("Profil"),
+                selectedColor: Theme.of(context).colorScheme.primary,
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
